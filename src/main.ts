503333fa79ed4b169d24f5bc1b0808c7d0945760
gyupp <kkb75281@gmail.com>
2024-11-04 14:29:39 +0900

    npm i skapi-js

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import './assets/main.less'
 
 import { createApp } from 'vue'
+import { Skapi } from "skapi-js"
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+const skapi = new Skapi("ap21T7fhHOcU2HgaCmGr", "5750ee2c-f7f7-43ff-b6a5-cce599d30101", { autoLogin: true }, {"hostDomain": "skapi.app","target_cdn": "d1wrj5ymxrt2ir"});
+
 app.use(router)
 
 app.mount('#app')
+
+export { skapi }
