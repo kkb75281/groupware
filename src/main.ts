bced4796c1592adc41fb8307ff7962b6e1128b90
gyupp <kkb75281@gmail.com>
2024-11-06 14:35:36 +0900

    change

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,16 @@ import { Skapi } from 'skapi-js';
 import App from './App.vue';
 import router from './router';
 
-const loginState = ref(false);
+// const loginState = ref(false);
 const app = createApp(App);
 const skapi = new Skapi(
-  'ap21T837jUF8IFyfR98Z',
-  'f498d188-1fa5-43e5-a32d-904d3e125983',
-  { autoLogin: true },
+  'ap21T7fhHOcU2HgaCmGr',
+  '5750ee2c-f7f7-43ff-b6a5-cce599d30101',
+  { autoLogin: false },
   { hostDomain: 'skapi.app', target_cdn: 'd1wrj5ymxrt2ir' }
 );
 
-skapi.login({
-  email: 'admin@email.com',
-  password: '123123'
-}).then(user => {
-  if(user) {
-    loginState.value = true;
-  }
-}).catch(err => {
-  loginState.value = false;
-  throw(err.message);
-});
+await skapi.__connection;
 
 app.use(router);
 
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,15 +1,76 @@
 <template lang="pug">
-h1 안녕하세요. 메인입니다.
+Header
+Navbar
+main#main
+    router-view
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { useRoute, useRouter } from 'vue-router';
-import Component from '@/components/component.vue';
+import Header from '@/components/header.vue';
+import Navbar from '@/components/navbar.vue';
 
 const router = useRouter();
 const route = useRoute();
 </script>
 
 <style scoped lang="less">
+#main {
+    padding-top: calc(var(--header-height));
+    padding-left: calc(var(--navbar-width));
+    transition: padding-left 0.15s linear;
 
+    .wrap {
+        padding: 20px 20px 0;
+    }
+}
+
+.fold {
+    #navbar {
+        width: var(--navbar-fold-width);
+    }
+
+    #main {
+        padding-left: calc(var(--navbar-fold-width));
+
+        .wrap {
+            padding: 20px 20px 0;
+        }
+    }
+
+    #header {
+        padding-left: var(--navbar-fold-width);
+    }
+}
+
+@media (max-width: 1200px) {
+    #header {
+        padding: 0 20px;
+    }
+
+    #navbar {
+        width: 0;
+    }
+
+    #main {
+        padding-left: 0;
+    }
+
+    .fold {
+        #header {
+            padding: 0 20px;
+        }
+
+        #navbar {
+            width: 0;
+        }
+
+        #main {
+            padding-left: 0;
+        }
+    }
+}
+@media (max-width: 768px) {
+    
+}
 </style>
