766af23351daa16c6115059030398a2b799b96db
gyupp <kkb75281@gmail.com>
2024-11-07 01:45:21 +0900

    change

--- src/views/Admin_list.vue
+++ src/views/Admin_list.vue
@@ -1,9 +1,20 @@
 <template lang="pug">
 h1 부서(회사) 목록
+
+br
+hr
+br
+
+
 </template>
 
 <script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router';
+import { ref } from 'vue';
+import { skapi } from '@/main';
 
+const router = useRouter();
+const route = useRoute();
 </script>
 
 <style scoped lang="less">
