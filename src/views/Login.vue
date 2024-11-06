13d95352750e679b080e1c59b81b5e6a3f7a8bab
alsdk9879 <alsdk9879@gmail.com>
2024-11-06 11:33:01 +0900

    design: login

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+#login
+	router-link(to="/")
+        //- img(src="@/assets/img/logo/symbol-logo.png")
+
+	form(@submit.prevent="")
+		.input-wrap
+			p.label.essential Email
+			input(type="email" placeholder="your@email.com")
+</template>
+
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router';
+import Component from '@/components/component.vue';
+
+const router = useRouter();
+const route = useRoute();
+</script>
+
+<style scoped lang="less">
+
+</style>
