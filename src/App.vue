4d5b2a7e5658edb5ebf0ed833b369fd6ca7b3da6
alsdk9879 <alsdk9879@gmail.com>
2024-11-06 12:55:27 +0900

    design: login

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
 <template lang="pug">
-Layout
+Login
 </template>
 
 <script setup lang="ts">
 import { useRoute, useRouter } from 'vue-router';
 import { onMounted, ref, computed, watch } from 'vue';
-import Layout from '@/components/layout.vue';
+import Login from '@/views/Login.vue';
 
 const router = useRouter();
 const route = useRoute();
 
-let navWidth = ref(0);
-
-onMounted(() => {
-    let navEl = document.getElementById('nav');
-    
-    if (navEl) {
-        navWidth.value = navEl.offsetWidth;
-
-        // Adjust width on window resize
-        window.addEventListener('resize', () => {
-            navWidth.value = navEl.offsetWidth;
-        });
-    }
-});
 </script>
 
 <style scoped lang="less">
