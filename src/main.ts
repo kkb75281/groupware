034800d17040f88520a728b173fc19b0bbbf7be1
gyupp <kkb75281@gmail.com>
2024-11-07 11:12:21 +0900

    change

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,20 @@ import { Skapi } from 'skapi-js';
 import App from './App.vue';
 import router from './router';
 
-// const loginState = ref(false);
-
 const app = createApp(App);
+const skapi = new Skapi(
+  'ap21T7fhHOcU2HgaCmGr',
+  '5750ee2c-f7f7-43ff-b6a5-cce599d30101',
+  { autoLogin: window.localStorage.getItem('remember') === 'true' },
+  { hostDomain: 'skapi.app', target_cdn: 'd1wrj5ymxrt2ir' }
+); // pb
+
 // const skapi = new Skapi(
-//   'ap21T7fhHOcU2HgaCmGr',
-//   '5750ee2c-f7f7-43ff-b6a5-cce599d30101',
+//   'ap21T837jUF8IFyfR98Z',
+//   'f498d188-1fa5-43e5-a32d-904d3e125983',
 //   { autoLogin: false },
 //   { hostDomain: 'skapi.app', target_cdn: 'd1wrj5ymxrt2ir' }
-// ); // pb
-
-const skapi = new Skapi(
-  'ap21T837jUF8IFyfR98Z',
-  'f498d188-1fa5-43e5-a32d-904d3e125983',
-  { autoLogin: false },
-  { hostDomain: 'skapi.app', target_cdn: 'd1wrj5ymxrt2ir' }
-);
+// );
 
 await skapi.__connection;
 
@@ -28,4 +26,4 @@ app.use(router);
 
 app.mount('#app');
 
-export { skapi, loginState };
+export { skapi };
