ceb560e85003a4f31ffa62e4ad380187bc98f700
gyupp <kkb75281@gmail.com>
2024-11-07 15:31:42 +0900

    header, navbar

---
+++ src/components/navbar.ts
@@ -0,0 +1,25 @@
+import { ref } from "vue";
+
+export const isFolded = ref(false); // 메뉴 접힘 상태
+export const isOpen = ref(false);   // 메뉴 열림 상태
+
+export function toggleNavbarFold() {
+    if (isOpen.value) {
+        isOpen.value = !isOpen.value
+        document.body.classList.toggle('open', isOpen.value)
+    } else {
+        isFolded.value = !isFolded.value;
+        document.body.classList.toggle('fold', isFolded.value);
+    }
+}
+
+export function toggleOpen() {
+    isOpen.value = !isOpen.value
+    document.body.classList.toggle('open', isOpen.value)
+}
+
+export function checkScreenWidth() {
+    if (window.innerWidth <= 1200) {
+        document.body.classList.remove('fold', isFolded.value);
+    }
+}
