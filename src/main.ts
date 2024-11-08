269bdd6fc269a646425d5257be9cdd2a5a5d3bcf
gyupp <kkb75281@gmail.com>
2024-11-08 18:03:52 +0900

    change service

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,19 @@ import router from './router';
 
 const app = createApp(App);
 
-// const skapi = new Skapi(
-//   'ap21T7fhHOcU2HgaCmGr',
-//   '5750ee2c-f7f7-43ff-b6a5-cce599d30101',
-//   { autoLogin: window.localStorage.getItem('remember') === 'true' },
-//   { hostDomain: 'skapi.app', target_cdn: 'd1wrj5ymxrt2ir' }
-// ); // pb
-
 const skapi = new Skapi(
-  'ap21T837jUF8IFyfR98Z',
-  'f498d188-1fa5-43e5-a32d-904d3e125983',
-  { autoLogin: false },
+  'ap21T7fhHOcU2HgaCmGr',
+  '5750ee2c-f7f7-43ff-b6a5-cce599d30101',
+  { autoLogin: window.localStorage.getItem('remember') === 'true' },
   { hostDomain: 'skapi.app', target_cdn: 'd1wrj5ymxrt2ir' }
-);
+); // pb
+
+// const skapi = new Skapi(
+//   'ap21T837jUF8IFyfR98Z',
+//   'f498d188-1fa5-43e5-a32d-904d3e125983',
+//   { autoLogin: false },
+//   { hostDomain: 'skapi.app', target_cdn: 'd1wrj5ymxrt2ir' }
+// );
 
 app.use(router);
 
