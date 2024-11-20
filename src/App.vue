b5c1cda1c775cde33a6528ec1cd1cb6ccb268981
gyupp <kkb75281@gmail.com>
2024-11-20 11:13:50 +0900

    skapi onLogin

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <template lang="pug">
-router-view(v-if="loaded")
+router-view
 </template>
 
 <script setup>
 import { useRoute, useRouter } from 'vue-router';
-import { ref } from 'vue';
-import { user, updateUser } from '@/user'
-
-import Login from '@/views/Login.vue';
 
 const router = useRouter();
 const route = useRoute();
-
-let loaded = ref(false);
-
-updateUser().then(r => {
-	if (!Object.keys(user).length) {
-		router.push('/login');
-	}
-}).finally(() => {
-    loaded.value = true;
-});
 </script>
 
 <style scoped lang="less">
