7b69872f23383186a8bfb1880f74c08e221947e9
gyupp <kkb75281@gmail.com>
2024-11-21 16:06:49 +0900

    fix: user login state

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,46 @@ import './assets/less/main.less';
 
 import { createApp, ref } from 'vue';
 import { Skapi } from 'skapi-js';
-import { loginCheck, initializeUserState } from './user';
+import { user, profileImage} from './user';
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
-initializeUserState();
+export let iwaslogged = ref(false);
+export let loaded = ref(false);
+
+export let loginCheck = async(profile: object | null, router: any) => {
+  if (profile) {
+    Object.assign(user, profile);
+    
+    if (user.picture) {
+      skapi
+      .getFile(user.picture, {
+        dataType: 'endpoint',
+      })
+      .then((res) => {
+        profileImage.value = res;
+      })
+      .catch((err) => {
+        window.alert('프로필 사진을 불러오는데 실패했습니다.');
+        throw err; // 의도적으로 에러 전달
+      });
+    } else {
+      profileImage.value = '';
+    }
+    
+    iwaslogged.value = true;
+  } else {   
+    if(iwaslogged.value) {
+      Object.assign(user, {});
+      iwaslogged.value = false;
+    }
+  }
+  console.log('profile', profile)
+  console.log('iwaslogged', iwaslogged.value)
+  loaded.value = true;
+};
 
 const skapi = new Skapi(
   'ap21UAo9MdRQtaQ8CmGr',
