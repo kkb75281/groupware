eef84a034d71e66b07bc3a03d0c11144499e3012
gyupp <kkb75281@gmail.com>
2024-12-09 13:05:01 +0900

    change skapi service

--- src/main.ts
+++ src/main.ts
@@ -63,7 +63,7 @@ export let loginCheck = async(profile: object | null) => {
 };
 
 const skapi = new Skapi(
-  'ap21UAo9MdRQtaQ8CmGr',
+  'ap21WQQ42ZUVa3GYCmGr',
   '5750ee2c-f7f7-43ff-b6a5-cce599d30101',
   { autoLogin: window.localStorage.getItem('remember') === 'true', eventListener: {onLogin: loginCheck} },
   { hostDomain: 'skapi.app', target_cdn: 'd1wrj5ymxrt2ir' }
