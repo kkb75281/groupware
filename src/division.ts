6cd7838f38d1c97034180a6617966a8627055b1e
gyupp <kkb75281@gmail.com>
2024-12-11 16:14:39 +0900

    update, mypage 초기 프로필 사진 사라지는 버그

--- src/division.ts
+++ src/division.ts
@@ -6,15 +6,21 @@ export let divisionNameList: Ref<{[key: string]: string}> = ref({});
 export let loading = ref(false);
 
 export async function getDivisionNames() {
-    await skapi.getRecords({
-        unique_id: '[division_name_list]',
-        table: {
-            name: 'divisionNames',
-            access_group: 1
-        },
-    }).then(r => {
-        if(r.list.length) {
-            divisionNameList.value = r.list[0].data;
-        }
-    })
+    let res;
+    try {
+        res = await skapi.getRecords({
+            unique_id: '[division_name_list]',
+            table: {
+                name: 'divisionNames',
+                access_group: 1
+            },
+        });
+    }
+    catch(e) {
+        alert('부서 이름 정보를 불러오는 중 오류가 발생했습니다.');
+        throw e;
+    }
+    if(res.list.length) {
+        divisionNameList.value = res.list[0].data;
+    }
 }
