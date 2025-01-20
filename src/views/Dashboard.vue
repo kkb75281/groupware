f616cc9cb6573f627e1a0071b05b28bc65ec0957
gyupp <kkb75281@gmail.com>
2025-01-20 18:41:06 +0900

    change

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,32 +1,39 @@
 <template lang="pug">
-ul.card-wrap.gmail(v-if="mailList && mailList.length")
+ul.card-wrap.gmail
 	li.card
-		.title-wrap
+		.title-wrap(:style="{ marginBottom: googleAccountCheck ? '1rem' : '0' }")
 			h3.title 
 				.icon.img
 					svg
 						use(xlink:href="@/assets/icon/material-icon.svg#icon-mail")
 				| 안읽은 메일
-			a.go-detail(:href="'https://mail.google.com/mail/u/0/#inbox'" target="_blank") 메일 더보기
+			a.go-detail(v-if="googleAccountCheck" :href="'https://mail.google.com/mail/u/0/#inbox'" target="_blank") 메일 더보기
 				.icon
 					svg
 						use(xlink:href="@/assets/icon/material-icon.svg#icon-arrow-forward-ios")
-		ul.unread-mail(v-if="mailList && mailList.length")
-			li.mail(v-for="mail in mailList" :key="mail.id" @click="readNoti(e, mail)")
-				.link
-					span.from {{ mail.from }}
-					span.mail-title {{ mail.subject }}
-					p.mail-cont {{ mail.snippet }}
-					span.attachment(v-if="mail.hasAttachment")
-						.icon
-							svg
-								use(xlink:href="@/assets/icon/material-icon.svg#icon-attach-file")
-					span.mail-date {{ mail.date }}
-		.empty(v-else)
-			.icon
-				svg
-					use(xlink:href="@/assets/icon/material-icon.svg#icon-error-outline")
-			| 더 이상 읽을 메일이 없습니다.
+			button.btn.outline(v-else @click="googleConnect")
+				img(src="@/assets/img/icon_google.svg")
+				| 구글 계정 연동하기
+		template(v-if="googleAccountCheck")
+			//- template(v-if="googleEmailUpdate")
+			//- 	Loading#loading
+			//- template(v-else)
+			ul.unread-mail(v-if="mailList && mailList.length")
+				li.mail(v-for="mail in mailList" :key="mail.id" @click="readNoti(e, mail)")
+					.link
+						span.from {{ mail.from }}
+						span.mail-title {{ mail.subject }}
+						p.mail-cont {{ mail.snippet }}
+						span.attachment(v-if="mail.hasAttachment")
+							.icon
+								svg
+									use(xlink:href="@/assets/icon/material-icon.svg#icon-attach-file")
+						span.mail-date {{ mail.date }}
+			.empty(v-else-if="mailList && !mailList.length")
+				.icon
+					svg
+						use(xlink:href="@/assets/icon/material-icon.svg#icon-error-outline")
+				| 더 이상 읽을 메일이 없습니다.
 
 ul.card-wrap
 	li.card
@@ -79,32 +86,84 @@ ul.card-wrap
 
 <script setup lang="ts">
 import { useRoute, useRouter } from 'vue-router';
-import { skapi, updateEmails } from "@/main";
+import { skapi, updateEmails, googleEmailUpdate } from "@/main";
 import { ref, watch, onMounted, onUnmounted } from 'vue';
 import { user } from "@/user";
 import { fetchGmailEmails } from "@/utils/mail";
 import { mailList, readAudit, readList, addEmailNotification, createReadListRecord } from "@/notifications";
 
+import Loading from '@/components/loading.vue';
+
 const router = useRouter();
 const route = useRoute();
 
+let loading = ref(false);
+let googleAccountCheck = sessionStorage.getItem('accessToken') ? true : false;
 let emailCheckInterval;  // interval 저장용 변수
 
-// // 이메일 업데이트
-// async function updateEmails() {
-//     const accessToken = sessionStorage.getItem('accessToken');
-    
-//     if (accessToken) {
-//         try {
-//             const res = await fetchGmailEmails(accessToken);
-// 			console.log('=== updateEmails === res : ', res);
-//             mailList.value = res;
+// 구글 계정 연동하기
+let googleConnect = async() => {
+	googleLogin();
+	// const GOOGLE_CLIENT_ID = '685505600375-tiheatfjtp0if764ri7ilop3o4nuhql3.apps.googleusercontent.com';
+	// const REDIRECT_URL = 'http://localhost:5173/login';
+	// const SCOPE = encodeURIComponent('https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/gmail.readonly');
 
-//             // console.log('=== updateEmails === res : ', res);
-//         } catch (error) {
-//             console.error('=== updateEmails === error : ', {error});
-//         }
+	// const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=token&scope=${SCOPE}`;
+	// // #ggl_skp_connect_ing
+	// window.location.href = authUrl;
+}
+
+// google login
+function googleLogin() {
+	loading.value = true;
+
+	const GOOGLE_CLIENT_ID = '685505600375-tiheatfjtp0if764ri7ilop3o4nuhql3.apps.googleusercontent.com';	// mina(broadwayinc.com) 계정으로 생성
+	// const GOOGLE_CLIENT_ID = '744531008220-v60665vfj19fgu1ajjlj0dj5sku7o4h8.apps.googleusercontent.com' // qb
+	const REDIRECT_URL = 'http://localhost:5173/login';
+
+	let rnd = Math.random().toString(36).substring(2); // Generate a random string
+	sessionStorage.setItem('oauth_state', rnd); // Store the state value in session storage
+
+	let url = 'https://accounts.google.com/o/oauth2/v2/auth';
+	url += '?client_id=' + GOOGLE_CLIENT_ID;
+	url += '&redirect_uri=' + encodeURIComponent(REDIRECT_URL);
+	url += '&response_type=token';
+	url += '&scope=' + encodeURIComponent('https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/gmail.readonly');
+	url += '&prompt=select_account';
+	url += '&state=' + encodeURIComponent(rnd); // Include the state parameter
+
+	window.location.href = url;
+}
+
+// async function handleOAuthCallback(hashValue) {  // 파라미터로 해시값을 받도록 수정
+//     const params = new URLSearchParams(hashValue.substring(1));
+//     const state = params.get('state');
+//     const storedState = sessionStorage.getItem('oauth_state');
+
+//     console.log('=== handleOAuthCallback === parms : ', params);
+//     console.log('=== handleOAuthCallback === state : ', state);
+//     console.log('=== handleOAuthCallback === storedState : ', storedState);
+
+//     loading.value = true;
+
+//     if (state !== storedState || !state || !storedState) {
+//         console.error('Invalid state parameter');
+//         return;
 //     }
+
+//     const OPENID_LOGGER_ID = 'by_skapi';
+//     const accessToken = params.get('access_token');
+//     sessionStorage.setItem('accessToken', accessToken);
+
+// 	console.log('=== handleOAuthCallback === accessToken : ', accessToken);
+// 	loading.value = false;
+
+
+//     skapi.openIdLogin({ id: OPENID_LOGGER_ID, token: accessToken }).then(u => {
+// 		console.log('=== handleOAuthCallback === u : ', u);
+//         window.location.href = '/';
+//     }).finally(() => {
+//     });
 // }
 
 let readNoti = async(e: Event, rt: any) => {
@@ -136,9 +195,9 @@ onMounted(async () => {
     await updateEmails();
     
     // 30초마다 이메일 업데이트
-    // emailCheckInterval = setInterval(() => {
-    //     updateEmails();
-    // }, 10000);
+    emailCheckInterval = setInterval(() => {
+        updateEmails();
+    }, 10000);
 });
 
 // 컴포넌트 언마운트 시 인터벌 정리
