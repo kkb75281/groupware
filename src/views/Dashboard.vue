21825e107b934e738b0df4829cf12f6e719dbd7f
gyupp <kkb75281@gmail.com>
2025-02-12 16:51:48 +0900

    change

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -138,9 +138,10 @@ let googleConnect = async() => {
 function googleLogin() {
 	loading.value = true;
 
-	const GOOGLE_CLIENT_ID = '685505600375-tiheatfjtp0if764ri7ilop3o4nuhql3.apps.googleusercontent.com';	// mina(broadwayinc.com) 계정으로 생성
-	// const GOOGLE_CLIENT_ID = '744531008220-v60665vfj19fgu1ajjlj0dj5sku7o4h8.apps.googleusercontent.com' // qb
-	const REDIRECT_URL = 'http://localhost:5173/login';
+	// const GOOGLE_CLIENT_ID = '685505600375-tiheatfjtp0if764ri7ilop3o4nuhql3.apps.googleusercontent.com';	// mina(broadwayinc.com) 계정으로 생성
+	const GOOGLE_CLIENT_ID = '541724791117-i8lafeboqua2i3ln8tmn17fm0tog445g.apps.googleusercontent.com' // qb
+	// const REDIRECT_URL = 'http://localhost:5173/login';
+	const REDIRECT_URL = 'https://groupwarefinal.skapi.app/login'; // qb
 
 	let rnd = Math.random().toString(36).substring(2); // Generate a random string
 	sessionStorage.setItem('oauth_state', rnd); // Store the state value in session storage
