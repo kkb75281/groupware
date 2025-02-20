2cf450f161b398dae675549c9b58d201897cd0c4
gyupp <kkb75281@gmail.com>
2025-02-20 10:27:14 +0900

    install vitepwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+	VitePWA({ registerType: 'autoUpdate' }),
     // // SVG 플러그인 추가
     // svgLoader({
     //   defaultImport: 'url'
