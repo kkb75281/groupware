64be7d7fe25ad7afbe5b8b87c0c44bf525b0e14c
gyupp <kkb75281@gmail.com>
2025-02-20 15:39:43 +0900

    pwa 모바일 환경 새로고침

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,31 @@ import { watch } from 'vue';
 const router = useRouter();
 const route = useRoute();
 
+let isRefreshing = false;
+
+document.addEventListener('touchstart', (e) => {
+    const startY = e.touches[0].pageY;
+    document.addEventListener('touchmove', onTouchMove);
+    document.addEventListener('touchend', onTouchEnd);
+
+    function onTouchMove(e) {
+        const currentY = e.touches[0].pageY;
+        if (currentY - startY > 100 && !isRefreshing) {
+            isRefreshing = true;
+            refreshPage();
+        }
+    }
+
+    function onTouchEnd() {
+        document.removeEventListener('touchmove', onTouchMove);
+        document.removeEventListener('touchend', onTouchEnd);
+    }
+});
+
+function refreshPage() {
+    window.location.reload();
+}
+
 watch(loaded, async(nv) => {
     if (!nv) return;
 
