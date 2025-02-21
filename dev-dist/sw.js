16cb0d05101b944b8a3f701f4a81bf1847d29728
gyupp <kkb75281@gmail.com>
2025-02-21 11:04:47 +0900

    update, service change

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.cnvf6ti96b8"
+    "revision": "0.kkti72uuijg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
