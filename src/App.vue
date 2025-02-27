8a86d0815ea5abd5414d670e68df48363a508f14
gyupp <kkb75281@gmail.com>
2025-02-27 20:44:43 +0900

    모바일 pwa 수동 새로고침 숨김

--- src/App.vue
+++ src/App.vue
@@ -13,24 +13,24 @@ const route = useRoute();
 
 let isRefreshing = false;
 
-document.addEventListener('touchstart', (e) => {
-    const startY = e.touches[0].pageY;
-    document.addEventListener('touchmove', onTouchMove);
-    document.addEventListener('touchend', onTouchEnd);
+// document.addEventListener('touchstart', (e) => {
+//     const startY = e.touches[0].pageY;
+//     document.addEventListener('touchmove', onTouchMove);
+//     document.addEventListener('touchend', onTouchEnd);
 
-    function onTouchMove(e) {
-        const currentY = e.touches[0].pageY;
-        if (currentY - startY > 100 && !isRefreshing) {
-            isRefreshing = true;
-            refreshPage();
-        }
-    }
+//     function onTouchMove(e) {
+//         const currentY = e.touches[0].pageY;
+//         if (currentY - startY > 100 && !isRefreshing) {
+//             isRefreshing = true;
+//             refreshPage();
+//         }
+//     }
 
-    function onTouchEnd() {
-        document.removeEventListener('touchmove', onTouchMove);
-        document.removeEventListener('touchend', onTouchEnd);
-    }
-});
+//     function onTouchEnd() {
+//         document.removeEventListener('touchmove', onTouchMove);
+//         document.removeEventListener('touchend', onTouchEnd);
+//     }
+// });
 
 function refreshPage() {
     window.location.reload();
