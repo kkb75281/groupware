4627ed9c90123095b1b1ce1672d18e89a8e0905e
alsdk9879 <alsdk9879@gmail.com>
2025-03-12 12:31:26 +0900

    change

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ export let buildTime = import.meta.env.VITE_BUILD_TIME;
 
 let serviceID = import.meta.env.VITE_SERVICE_ID;
 
-console.log("바뀐 버전 입니다. 0312 12:14");
+console.log("바뀐 버전 입니다. 0312 12:31");
 
 const skapi = new Skapi(
 	import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_OWNER_ID,
