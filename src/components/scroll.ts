8d7d38fd181e1430d70c148afc661e19da9a059c
alsdk9879 <alsdk9879@gmail.com>
2025-03-25 22:42:11 +0900

    working: fetch more

---
+++ src/components/scroll.ts
@@ -0,0 +1,10 @@
+// 스크롤 이벤트
+// window.addEventListener('scroll', () => {
+//     if (isFetching || !hasMore) {
+//         return
+//     ｝
+
+//     if ((window. innerHeight + window.scrollY) ›= document.body.offsetHeight) {
+//         fetchData ();
+//     }
+// })
