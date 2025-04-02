040685911c8cb1943f85bb726615a21c1ba85d3a
alsdk9879 <alsdk9879@gmail.com>
2025-04-02 10:13:38 +0900

    fix: 메뉴명 통일 및 오타 수정

--- src/views/approval/Approval.vue
+++ src/views/approval/Approval.vue
@@ -41,7 +41,7 @@ ul.card-wrap
 			.icon.img
 				svg
 					use(xlink:href="@/assets/icon/material-icon.svg#icon-tag")
-			h4.name 수신참조
+			h4.name 수신 참조함
 			.btn-wrap
 				p.btn-go 바로가기
 				.icon
@@ -53,7 +53,7 @@ ul.card-wrap
 			.icon.img
 				svg
 					use(xlink:href="@/assets/icon/material-icon.svg#icon-star")
-			h4.name 종요결재
+			h4.name 중요 결재함
 			.btn-wrap
 				p.btn-go 바로가기
 				.icon
